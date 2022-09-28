d50c11af2d7432b604ae7000128d53481da1b594
Camilo Faria <camilozabaleta65@gmail.com>
2022-09-27 19:49:29 -0500

    Update config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -50,16 +50,16 @@ tag.connect_signal("request::default_layouts", function()
         awful.layout.suit.tile,
         awful.layout.suit.floating,
         awful.layout.suit.tile.left,
-        awful.layout.suit.tile.bottom,
-        awful.layout.suit.tile.top,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.tile.top,
         awful.layout.suit.fair,
-        awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.fair.horizontal,
         awful.layout.suit.spiral,
-        awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.max,
+        -- awful.layout.suit.spiral.dwindle,
+        -- awful.layout.suit.max,
         awful.layout.suit.max.fullscreen,
-        awful.layout.suit.magnifier,
-        awful.layout.suit.corner.nw,
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.corner.nw,
     })
 end)
 
