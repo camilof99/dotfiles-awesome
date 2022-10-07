468cb495af671defba9865184e63b36348f98bbc
Camilo Faria <camilozabaleta65@gmail.com>
2022-10-07 11:21:30 -0500

    Update config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -36,6 +36,39 @@ naughty.connect_signal("request::display_error", function(message, startup)
 end)
 -- }}}
 
+
+-- Credits forum reddit...
+awesome.connect_signal('exit', function(reason_restart)
+    if not reason_restart then return end
+ 
+    local file = io.open('/tmp/awesomewm-last-selected-tags', 'w+')
+ 
+    for s in screen do
+       file:write(s.selected_tag.index, '\n') 
+    end
+ 
+    file:close()
+ end)
+ 
+awesome.connect_signal('startup', function()
+    local file = io.open('/tmp/awesomewm-last-selected-tags', 'r')
+    if not file then return end
+ 
+    local selected_tags = {}
+ 
+    for line in file:lines() do
+       table.insert(selected_tags, tonumber(line))
+    end
+    
+    for s in screen do
+       local i = selected_tags[s.index]
+       local t = s.tags[i]
+       t:view_only()
+    end
+ 
+    file:close()
+ end)
+
 --beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.init(gears.filesystem.get_configuration_dir() .. "mytheme.lua")
 
