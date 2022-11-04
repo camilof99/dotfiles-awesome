7837ed18389f67fded075237b5ac630fcdf1e584
Camilo Faria <48364238+camilof99@users.noreply.github.com>
2022-11-04 12:12:07 -0500

    Update apps.lua

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -9,7 +9,7 @@ return {
 		--- Default terminal emulator
 		terminal = "kitty",
 		code_editor = "code",
-		web_browser = "microsoft-edge-stable",
+		web_browser = "firefox",
 		music_player = "kitty ncmpcpp",
 		file_manager = "nautilus",
 		app_launcher = "rofi -no-lazy-grab -show drun -modi drun -theme " .. config_dir .. "scripts/rofi.rasi",
